e6b469cf984a34da51857a94928d480621d3522f
YUK1 <116713322+YUK1-0000@users.noreply.github.com>
2023-11-10 12:33:38 +0900

    部屋番号が入力されていないときにHost/Joinボタンを無効化

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -2,8 +2,14 @@
 extends Control
 
 
+@onready var create: Button = %Create
+@onready var connect: Button = %Connect
+
+
 func _process(_delta: float) -> void:
 	Game.port = int(%RoomNum.text)
+	create.disabled = not Game.port
+	connect.disabled = not Game.port
 
 
 func _on_create_pressed() -> void:
